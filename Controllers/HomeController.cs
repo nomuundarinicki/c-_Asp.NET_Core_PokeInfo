f31f3b300f7a22b67ba6d908e38c635edab424ef
Nick Chimedbavuu <nickchimedbavuu@Nickis-MacBook-Pro.local>
2017-08-29 16:03:30 -0700

    working in progress

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace PokeInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("pokemon/{pokeid}")]
+        public IActionResult QueryPoke(int pokeid)
+         {
+        var PokeInfo = new Dictionary<string, object>();
+        WebRequest.GetPokemonDataAsync(pokeid, ApiResponse =>
+            {
+                PokeInfo = ApiResponse;
+            }
+        ).Wait();
+        // Other code
+        }
+
+    }
+
+}
